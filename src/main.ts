b261e3d87138bc27c64380854c640e53e42327ef
N0tail12 <N0tail12>
2022-04-05 20:22:18 +0700

    feat: add font-awsome to get the goto arrow

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-createApp(App).use(store).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+createApp(App)
+  .use(store)
+  .component("fa", FontAwesomeIcon)
+  .mount("#app");
